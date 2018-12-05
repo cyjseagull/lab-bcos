c0fef9a1bdcf23e81f06e5242d0471e979729732
cyjseagull <1547158861@qq.com>
2018-12-06 00:17:45 +0800

    implement leaked files

---
+++ libdevcore/Log.cpp
@@ -0,0 +1,7 @@
+#include "Log.h"
+namespace dev
+{
+std::string const FileLogger = "FileLogger";
+boost::log::sources::severity_channel_logger_mt<boost::log::trivial::severity_level, std::string>
+    FileLoggerHandler(boost::log::keywords::channel = FileLogger);
+}  // namespace dev
